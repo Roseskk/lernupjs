4f0fa2260145d3de42b485a20c5c1e89937be0c0
Roseskk <oleg419583@yandex.ru>
2022-03-17 23:54:29 +0300

    hw3

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
+let button = document.querySelector('.button');
+let root = document.querySelector('.arenas')
+
 
 const player1 = {
+    player : 1,
     name : 'Scorpion',
     hp : 100,
     img : 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif',
@@ -8,39 +12,40 @@ const player1 = {
 }
 
 const player2 = {
+    player : 2,
     name : 'Kitana',
     hp : 100,
-    img : 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif',
+    img : 'http://reactmarathon-api.herokuapp.com/assets/kitana.gif',
     weapon : ['ak-47','m4a1','mp5','knife'],
     attack : ()=> { console.log( this.name + 'Fight...') }
 }
 
-function createPlayer(player,name1,hp,image) {
+function createPlayer(playerClass,player) {
     let player1 = document.createElement('div');
-    player1.classList.add(player)
+    player1.classList.add(playerClass)
 
     let progressBar = document.createElement('div');
     progressBar.className = 'progressbar';
 
     let life = document.createElement('div');
     life.className = 'life';
-    life.innerHTML = hp;
+    life.innerHTML = player.hp;
     life.style.width = '100%';
 
     let name = document.createElement('div');
     name.className = 'name';
-    name.innerHTML = name1;
+    name.innerHTML = player.name;
 
     let character = document.createElement('div');
     character.className = 'character';
 
     let img = document.createElement('img');
-    img.src = image;
+    img.src = player.img;
 
     let root = document.querySelector('.arenas')
     root.appendChild(player1);
 
-    let $player1 = document.querySelector(`${'.'+player}`);
+    let $player1 = document.querySelector(`${'.'+playerClass}`);
 
     $player1.appendChild(progressBar);
     $player1.appendChild(character);
@@ -51,5 +56,35 @@ function createPlayer(player,name1,hp,image) {
     character.appendChild(img)
 }
 
-createPlayer('player1','Scorpion','80','http://reactmarathon-api.herokuapp.com/assets/scorpion.gif');
-createPlayer('player2','Kitana','100','http://reactmarathon-api.herokuapp.com/assets/kitana.gif');
+function changeHp(player) {
+    let life = document.querySelector('.player' + player.player + ' '+  '.life')
+    player.hp = player.hp - Math.floor(Math.random() * 30)
+    life.innerHTML = player.hp
+    if (player.hp <=0) {
+        player.hp = 0;
+        console.log('asd')
+        button.disabled = true;
+        if (player1.hp <= 0 && player2.hp > 0) {
+            let winner = document.createElement('div')
+            winner.innerHTML = ' PLAYER 1 WIN'
+            winner.style.color = 'red'
+            winner.style.position = 'absolute'
+            root.appendChild(winner);
+        } else if (player2.hp <= 0 && player1.hp >0) {
+            let winner = document.createElement('div')
+            winner.innerHTML = ' PLAYER 2 WIN'
+            winner.style.color = 'red'
+            winner.style.position = 'absolute'
+            root.appendChild(winner);
+        }
+    }
+    life.style.width = player.hp
+}
+
+document.addEventListener('click', function (){
+    changeHp(player1);
+    changeHp(player2);
+})
+
+createPlayer('player1',player1);
+createPlayer('player2',player2);
